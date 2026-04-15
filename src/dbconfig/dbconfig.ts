91be8af494af22c3c30180c17623ee3ccd2185a6
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-04-15 11:55:14 +0530

    new

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -3,24 +3,41 @@ import path from "path";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT) || 5432,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+
+//   synchronize: false,
+//   logging: true,
+
+//   entities: [path.join(__dirname, "../entities/**/*.ts")],
+//   migrations: [path.join(__dirname, "../migrations/**/*.ts")],
+//   subscribers: [path.join(__dirname, "../subscribers/**/*.ts")],
+
+//   migrationsRun: true,
+// });
+
+
 export const AppDataSource = new DataSource({
   type: "postgres",
 
   url: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV !== "production" ? { rejectUnauthorized: false } : undefined,
 
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
   synchronize: false,
-  logging: process.env.NODE_ENV === "production",
+  logging: false,
 
-  entities: [path.join(__dirname, "../entities/**/*.{js,ts}")],
-  migrations: [path.join(__dirname, "../migrations/**/*.{js,ts}")],
-  subscribers: [path.join(__dirname, "../subscribers/**/*.{js,ts}")],
+  entities: [path.join(__dirname, "../entities/**/*.js")],
+  migrations: [path.join(__dirname, "../migrations/**/*.js")],
+  subscribers: [path.join(__dirname, "../subscribers/**/*.js")],
 
   migrationsRun: true,
 });
